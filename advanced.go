396c477136e215d8fb2787edb91af186fe1ef295
Tacey Wong <xinyong.wang@qq.com>
2020-01-08 22:30:07 +0800

    add package-name first

--- advanced.go
+++ advanced.go
@@ -1,6 +1,6 @@
 // Licensed under the MIT license that can be found in the LICENSE file.
 
-
+package xstr
 // PORT https://github.com/seatgeek/fuzzywuzzy
 
 // 
--- random.go
+++ random.go
@@ -1,3 +1,5 @@
 // Licensed under the MIT license that can be found in the LICENSE file.
 
+package xstr
+
 // TODO: Add some string random generation function
