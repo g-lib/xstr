d3a55e9928f715844a959c69ca7bb7c90acabb6d
TaceyWong <xinyong.wang@xtalpi.com>
2020-01-08 10:12:18 +0800

    integration strings-package's  method/function

---
+++ integration.go
@@ -0,0 +1,56 @@
+// Licensed under the MIT license that can be found in the LICENSE file.
+
+package xstr
+
+import "strings"
+
+var Compare = strings.Compare
+var Contains = strings.Contains
+var  ContainsAny = strings.ContainsAny
+var ContainsRune = strings.ContainsRune
+var Count = strings.Count
+var EqualFold = strings.EqualFold
+var Fields = strings.Fields
+var Fieldsvar = strings.Fields
+var HasPrefix = strings.HasPrefix
+var HasSuffix = strings.HasSuffix
+var Index = strings.Index
+var IndexAny = strings.IndexAny
+var IndexByte = strings.IndexByte
+var Indexvar = strings.IndexFunc
+var IndexFunc = strings.IndexFunc
+var IndexRune = strings.IndexRune
+var Join = strings.Join
+var LastIndex = strings.LastIndex
+var LastIndexAny = strings.LastIndexAny
+var LastIndexByte = strings.LastIndexByte
+var LastIndexvar = strings.LastIndexFunc
+var LastIndexFunc = strings.LastIndexFunc
+var Map = strings.Map
+var Repeat = strings.Repeat
+var Replace = strings.Replace
+var ReplaceAll = strings.ReplaceAll
+var Split = strings.Split
+var SplitAfter = strings.SplitAfter
+var SplitAfterN = strings.SplitAfterN
+var SplitN = strings.SplitN
+var Title = strings.Title
+var ToLower = strings.ToLower
+var ToLowerSpecial = strings.ToLowerSpecial
+var ToTitle = strings.ToTitle
+var ToTitleSpecial = strings.ToTitleSpecial
+var ToUpper = strings.ToUpper
+var ToUpperSpecial = strings.ToUpperSpecial
+var ToValidUTF8 = strings.ToValidUTF8
+var Trim = strings.Trim
+var Trimvar = strings.TrimFunc
+var TrimFunc = strings.TrimFunc
+var TrimLeft = strings.TrimLeft
+var TrimLeftvar = strings.TrimLeftFunc
+var TrimLeftFunc = strings.TrimLeftFunc
+var TrimPrefix = strings.TrimPrefix
+var TrimRight = strings.TrimRight
+var TrimRightvar = strings.TrimRightFunc
+var TrimRightFunc = strings.TrimRightFunc
+var TrimSpace = strings.TrimSpace
+var TrimSuffix = strings.TrimSuffix
