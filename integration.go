af14c0551afd1430beffcfd80b06a50826d2b4d3
TaceyWong <xinyong.wang@xtalpi.com>
2020-12-24 16:17:36 +0800

    strings -> xstr

--- integration.go
+++ integration.go
@@ -4,53 +4,152 @@ package xstr
 
 import "strings"
 
+// Compare equal to strings.Compare
 var Compare = strings.Compare
+
+// Contains equal to strings.Contains
 var Contains = strings.Contains
-var  ContainsAny = strings.ContainsAny
+
+// ContainsAny equal to strings.ContainsAny
+var ContainsAny = strings.ContainsAny
+
+// ContainsRune equal to strings.ContainsRune
 var ContainsRune = strings.ContainsRune
+
+// Count equal to strings.Count
 var Count = strings.Count
+
+// EqualFold equal to strings.EqualFold
 var EqualFold = strings.EqualFold
+
+// Fields equal to strings.Fields
 var Fields = strings.Fields
+
+// Fieldsvar equal to strings.Fields
 var Fieldsvar = strings.Fields
+
+// HasPrefix equal to strings.HasPrefix
 var HasPrefix = strings.HasPrefix
+
+// HasSuffix equal to strings.HasSuffix
 var HasSuffix = strings.HasSuffix
+
+// Index equal to strings.Index
 var Index = strings.Index
+
+// IndexAny equal to strings.IndexAny
 var IndexAny = strings.IndexAny
+
+// IndexByte equal to strings.IndexByte
 var IndexByte = strings.IndexByte
+
+// Indexvar equal to strings.IndexFunc
 var Indexvar = strings.IndexFunc
+
+// IndexFunc equal to strings.IndexFunc
 var IndexFunc = strings.IndexFunc
+
+// IndexRune equal to strings.IndexRune
 var IndexRune = strings.IndexRune
+
+// Join equal to strings.Join
 var Join = strings.Join
+
+// LastIndex equal to strings.LastIndex
 var LastIndex = strings.LastIndex
+
+// LastIndexAny equal to strings.LastIndexAny
 var LastIndexAny = strings.LastIndexAny
+
+// LastIndexByte equal to strings.LastIndexByte
 var LastIndexByte = strings.LastIndexByte
+
+// LastIndexvar equal to strings.LastIndexFunc
 var LastIndexvar = strings.LastIndexFunc
+
+// LastIndexFunc equal to strings.LastIndexFunc
 var LastIndexFunc = strings.LastIndexFunc
+
+// Map equal to strings.Map
 var Map = strings.Map
+
+// Repeat equal to strings.Repeat
 var Repeat = strings.Repeat
+
+// Replace equal to strings.Replace
 var Replace = strings.Replace
+
+// ReplaceAll equal to strings.ReplaceAll
 var ReplaceAll = strings.ReplaceAll
+
+// Split equal to strings.Split
 var Split = strings.Split
+
+// SplitAfter equal to strings.SplitAfter
 var SplitAfter = strings.SplitAfter
+
+// SplitAfterN equal to strings.SplitAfte
 var SplitAfterN = strings.SplitAfterN
+
+// SplitN equal to strings.SplitN
 var SplitN = strings.SplitN
+
+// Title equal to strings.Title
 var Title = strings.Title
+
+// ToLower equal to strings.ToLower
 var ToLower = strings.ToLower
+
+// ToLowerSpecial equal to strings.ToLowerSpecial
 var ToLowerSpecial = strings.ToLowerSpecial
+
+// ToTitle equal to strings.ToTitle
 var ToTitle = strings.ToTitle
+
+// ToTitleSpecial equal to strings.ToTitleSpecial
 var ToTitleSpecial = strings.ToTitleSpecial
+
+// ToUpper equal to strings.ToUpper
 var ToUpper = strings.ToUpper
+
+// ToUpperSpecial equal to strings.ToUpperSpecial
 var ToUpperSpecial = strings.ToUpperSpecial
+
+// ToValidUTF8 equal to strings.ToValidUTF8
 var ToValidUTF8 = strings.ToValidUTF8
+
+// Trim equal to strings.Trim
 var Trim = strings.Trim
+
+// Trimvar equal to strings.TrimFunc
 var Trimvar = strings.TrimFunc
+
+// TrimFunc equal to strings.TrimFunc
 var TrimFunc = strings.TrimFunc
+
+// TrimLeft equal to strings.TrimLeft
 var TrimLeft = strings.TrimLeft
+
+// TrimLeftvar equal to strings.TrimLeftFunc
 var TrimLeftvar = strings.TrimLeftFunc
+
+// TrimLeftFunc euqal to strings.TrimLeftFunc
 var TrimLeftFunc = strings.TrimLeftFunc
+
+// TrimPrefix euqal to strings.TrimPrefix
 var TrimPrefix = strings.TrimPrefix
+
+// TrimRight euqal to strings.TrimRight
 var TrimRight = strings.TrimRight
+
+// TrimRightvar euqal to strings.TrimRightFunc
 var TrimRightvar = strings.TrimRightFunc
+
+// TrimRightFunc euqal to strings.TrimRightFunc
 var TrimRightFunc = strings.TrimRightFunc
+
+// TrimSpace equal to strings.TrimSpace
 var TrimSpace = strings.TrimSpace
+
+// TrimSuffix equal to strings.TrimSuffix
 var TrimSuffix = strings.TrimSuffix
